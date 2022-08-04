cc300f3c538725535251ca7db2f74e6d67363c41
AlisonS31 <105736739+AlisonS31@users.noreply.github.com>
2022-08-03 19:50:44 -0600

    Reporte con Jasper Report

---
+++ src/main/java/com/tienda/controller/ReportController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.controller;
+
+import com.tienda.service.PersonaService;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author alison
+ */
+@RestController
+public class ReportController {
+    @Autowired
+    private PersonaService personaservice;
+
+    @GetMapping("/crearReporte")
+    public String generatedPdf() throws FileNotFoundException, JRException{
+
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(personaservice.getAllPersona());
+        JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream("C:\\Users\\ad\\Documents\\GitHub\\Tienda\\src\\main\\resources\\Tienda.jrxml"));
+
+        HashMap<String,Object> map=new HashMap<>();
+        JasperPrint report = JasperFillManager.fillReport(compileReport, map, beanCollectionDataSource);
+        JasperExportManager.exportReportToPdfFile(report,"ReporteFinal.pdf");
+
+        return "Reporte creado con exito";
+    }
+}
+
